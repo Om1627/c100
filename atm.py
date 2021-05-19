810e390ca8719df959973929eaee2afaa7994b7e
Om1627 <60969676+Om1627@users.noreply.github.com>
2021-05-19 16:36:34 +0530

    Add files via upload

--- atm.py
+++ atm.py
@@ -3,14 +3,25 @@ class atm(object):
         self.cardnumber=cardnumber
         self.pin=pin
         
+    def main(self):
+        accNo=input("Enter your account number ")
+        pin=input("Enter your pin ")
 
-    def CashWithdrawl(self,amount):
-       amount=input("Enter amount of cash to be withdrawed ")
-       print("Cash withdrawed ",amount)
+    
+    def CashWithdrawl(self):
+        
+       amount=int(input("Enter amount of cash to be withdrawed "))
+       newamount=2000-amount
+       print("You have withdrawed ",amount,"Your remaining balance is ",newamount)
 
     def balanceEnquiry(self):
         print("Your account balance is 2000 ")
 
    
 Atm=atm(12345678,1234)
-print(Atm.CashWithdrawl(0))
+Atm.main()
+x=int(input("Type 1 for withdrawing cash and 2 for balance enquiry "))
+if(x==1):
+    Atm.CashWithdrawl()
+else:
+    print(Atm.balanceEnquiry())    
